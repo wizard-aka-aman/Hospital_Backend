d734c9566a97764d2e4c5360a80c39e9bc04c0bc
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-09-17 01:29:03 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,7 +1,12 @@
 export const generateToken = (user,message,statusCode,res)=>{
 const token = user.generateJsonWebToken();
 const cookieName  = user.role ==="Admin" ? "adminToken" : "patientToken";
-res.status(statusCode).cookie(cookieName , token).json({
+    const options = {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None"
+    }
+res.status(statusCode).cookie(cookieName , token , options).json({
     success : true,
     message,
     user,
